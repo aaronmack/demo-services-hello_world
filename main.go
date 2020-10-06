5c4722af594fc09e89e75e1f3e9b0c6aad08caed
Aaron <lilong999000@gmail.com>
2020-10-06 19:21:39 +0800

    [added] wercker.ci

---
+++ main.go
@@ -0,0 +1,30 @@
+// run: go get github.com/urfave/negroni
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/urfave/negroni"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	hostString := fmt.Sprintf(":%s", port)
+	n.Run(hostString)
+}
+
+func hello(res http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(res, "Hello from Go!")
+}
